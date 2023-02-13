f6a0ef46b165a17ad2fed2e924ed554fc8034557
ngachadavid <davidngacha01@gmail.com>
2023-02-13 13:07:29 +0300

    Done

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,82 @@
-# your code goes here
+require 'pry'
+
+class Person
+    attr_reader :name, :happiness, :hygiene
+    attr_accessor :bank_account
+
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(happiness)
+        if happiness > 10
+            @happiness = 10
+        elsif happiness < 0
+            @happiness = 0
+        else
+            @happiness = happiness
+        end
+    end
+
+    def hygiene=(hygiene)
+        if hygiene > 10
+            @hygiene = 10
+        elsif hygiene < 0
+            @hygiene = 0
+        else
+            @hygiene = hygiene
+        end
+    end
+
+    def happy?
+        self.happiness > 7
+    end
+
+    def clean?
+        self.hygiene > 7
+    end
+
+    def get_paid(amount)
+        self.bank_account += amount
+        "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.hygiene -= 3
+        self.happiness += 2
+        "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        friend.happiness += 3
+        self.happiness += 3
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(friend, topic)
+        case topic
+        when "politics"
+            [self, friend].each { |person| person.happiness -= 2 }
+            "blah blah partisan blah lobbyist"
+        when "weather"
+            [self, friend].each { |person| person.happiness += 1 }
+            "blah blah sun blah rain"
+        else
+            "blah blah blah blah blah"
+        end
+
+
+    end
+
+ngacha = Person.new(name)
+
+end
